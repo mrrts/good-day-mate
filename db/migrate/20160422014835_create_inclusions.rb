94485f7ef95162fbbaac36bba8bf661dece100f5
Ryan Smith <mrryantsmith@gmail.com>
2016-04-22 13:16:49 -0500

    Update 20160422014835_create_inclusions.rb

--- db/migrate/20160422014835_create_inclusions.rb
+++ db/migrate/20160422014835_create_inclusions.rb
@@ -2,7 +2,6 @@ class CreateInclusions < ActiveRecord::Migration
   def change
     create_table :inclusions do |t|
       t.integer :stream_id
-      t.integer :current_id
       t.integer :order
       t.integer :includable_id
       t.string :includable_type
