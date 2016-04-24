b912567c6c3e900ccd923e77825bf98d3ff5ff16
Heather Conklin & Taylor Daugherty <pair+heath3conk.taylordaug@devbootcamp.com>
2016-04-24 13:29:56 -0500

    coffee cup

--- app/assets/javascripts/components/ReviewScreen.js.jsx
+++ app/assets/javascripts/components/ReviewScreen.js.jsx
@@ -21,7 +21,7 @@ var ReviewScreen = React.createClass({
       <div className='container'>
         <ul>
           <li className='card'>You were feeling {this.state.feeling}</li>
-          <li className='card'>You were thankful for {this.state.thankful1}, {this.state.thankful2}, {this.state.thankful3}</li>
+          <li className='card'>You were thankful for {this.state.thankful1}, {this.state.thankful2}, and {this.state.thankful3}</li>
           <li className='card'>You are looking forward to {this.state.horizon}</li>
           <a onClick={this.handleStartClick} className='btn-large waves-effect waves-light' id='start-day-button'>Start Your Day</a>
         </ul>
