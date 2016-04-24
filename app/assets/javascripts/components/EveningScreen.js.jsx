fcaf348fc2bdc8044cb1aa89fc785c2843c7f064
Heather Conklin & Taylor Daugherty <pair+heath3conk.taylordaug@devbootcamp.com>
2016-04-24 16:30:30 -0500

    fix swipe on review

--- app/assets/javascripts/components/EveningScreen.js.jsx
+++ app/assets/javascripts/components/EveningScreen.js.jsx
@@ -3,9 +3,9 @@ var EveningScreen = React.createClass({
     return {}
   },
   componentDidMount: function(){
+    window.mySwipe = Swipe(document.getElementById('slider'));
     $.get('/check_ins/new', function(response){
      var auth_token = $(response).find('input[name="authenticity_token"]').val()
-      console.log(auth_token);
       this.setState({authenticity_token: auth_token})
     }.bind(this))
   },
@@ -24,21 +24,31 @@ var EveningScreen = React.createClass({
     return (
       <div className='container' onSubmit={this.handleFormSubmit}>
         <form acceptCharset="UTF-8" method="post"><input name="utf8" type="hidden" value="&#x2713;" /><input type="hidden" name="authenticity_token" value={this.state.authenticity_token} />
-        <div className='card'>
-          <label htmlFor="check_in_feeling">I am feeling</label>
-          <input type="text" name="check_in[feeling]" id="check_in_feeling" />
-        </div>
-        <div className='card'>
-          <label htmlFor="check_in_thankful1">I am thankful for</label>
-          <input type="text" name="check_in[thankful1]" id="check_in_thankful1" />
-          <input type="text" name="check_in[thankful2]" id="check_in_thankful2" />
-          <input type="text" name="check_in[thankful3]" id="check_in_thankful3" />
-        </div>
-        <div className='card'>
-          <label htmlFor="check_in_horizon">I am looking forward to</label>
-          <input type="text" name="check_in[horizon]" id="check_in_horizon" />
-          <input type="submit" name="commit" value="Build Tomorrow" />
-        </div>
+          <div id="slider" className="swipe">
+            <div className='swipe-wrap'>
+              <div>
+                <div className='card'>
+                  <input type="text" name="check_in[feeling]" id="check_in_feeling" />
+                  <label htmlFor="check_in_feeling">I am feeling</label>
+                </div>
+              </div>
+              <div>
+                <div className='card'>
+                  <input type="text" name="check_in[thankful1]" id="check_in_thankful1" />
+                  <input type="text" name="check_in[thankful2]" id="check_in_thankful2" />
+                  <input type="text" name="check_in[thankful3]" id="check_in_thankful3" />
+                  <label htmlFor="check_in_thankful1">I am thankful for</label>
+                </div>
+              </div>
+              <div>
+                <div className='card'>
+                  <input type="text" name="check_in[horizon]" id="check_in_horizon" />
+                  <label htmlFor="check_in_horizon">I am looking forward to</label>
+                </div>
+                  <input className="btn-large waves-effect waves-light" type="submit" name="commit" value="Build Tomorrow" />
+              </div>
+            </div>
+          </div>
         </form>
       </div>
     )
