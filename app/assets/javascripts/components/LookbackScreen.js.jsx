0eca1cf52aaaa8df229576ecd3afbe93435e5710
Heather Conklin <heath3conk@gmail.com>
2016-04-27 10:50:41 -0500

    Some work on lookback data display

--- app/assets/javascripts/components/LookbackScreen.js.jsx
+++ app/assets/javascripts/components/LookbackScreen.js.jsx
@@ -7,20 +7,51 @@ var LookbackScreen = React.createClass({
   handleStartClick: function(event){
     this.props.onUpdate('start')
   },
+
+  gratitudesList: function() {
+    var grats = this.state.gratitudes;
+    if (grats) {
+      grats.map(function(gratitude, i) {
+        return (
+          <li data-key={i} key={i}>
+             Gratitude: {gratitude[0]}, frequency: {gratitude[1]}
+          </li>
+        )
+      })
+    } else {
+      "UNKNOWN"
+    }
+    // var firstGrat = grats ? grats[0] : "UNKNOWN"
+    // console.log("firstGrat " + firstGrat);
+    return grats
+  },
+
   componentDidMount: function() {
     $.get("/users/history", function(response){
+      console.log(response)
       this.setState({
-        gratitudes: response.gratitudes
-      });
+        gratitudes: response.gratitudes,
+        feelings: response.feelings,
+        horizons: response.horizons
+      })
     }.bind(this))
   },
+
+  // var grats: this.state.gratitudes,
+  // var feels: this.state.feelings,
+  // var tomorrows: this.state.horizons,
+
+ 
+
   render: function(){
     return (
       <div className='container flow-text'>
           <div className="card">
             <div className="card-content">
-              <p>Here I am!</p>
-              <p>{this.props.gratitudes}</p>
+              I'm grateful for: 
+              <ul>
+                {this.gratitudesList()}
+              </ul>
             </div>
           </div>         
       </div>
